0dd65efdbd7ac842a565a11109a44962a286d5b7
Emma <emma@example.com>
2026-07-25 20:10:20 -0600

    Agregue album a la api

--- backend/api.py
+++ backend/api.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/cancion")
 def get_cancion():
     return {"album": "Porfiado- Cuarteto de Nos"}
+
+@app.get("/album")
+def get_album():
+    return {"cancion": "Inverno del 92 - Cuarteto de nos"}
